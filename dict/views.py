18f47468fe3fe314453f10ae6c820d9d0df353d1
lucif3r <phucphuong1293@gmail.com>
2016-09-03 01:47:44 +0300

    print out the word's inflection..

--- dict/views.py
+++ dict/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Dictionary
-from .magic import Word
+from .magic import Word, Noun
 
 
 def index(request):
@@ -13,6 +13,9 @@ def search_form(request):
 def search(request):
     if 'q' in request.GET and request.GET['q']:
         q = request.GET['q']
+
+        noun = Noun(q)
+        noun.fill_the_table()
         normal = Word(q)
         # Word.normalize(normal)
         normal.normalize()
@@ -20,5 +23,5 @@ def search(request):
         similar_words = {}
         if(len(words) == 0):
             similar_words = Dictionary.objects.filter(word__istartswith=q)
-        return render(request, 'dict/search_results.html', {'words': words, 'query': q, 'similar_words': similar_words})
+        return render(request, 'dict/search_results.html', {'words': words, 'query': q, 'similar_words': similar_words, 'table_inflection': noun._table})
     return render(request, 'dict/search_form.html', {'error_message': "Please submit the search form!"})
